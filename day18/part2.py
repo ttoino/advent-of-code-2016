93d7328e2dd1041ee04a34d1ca52f56f3a8e45b7
toino <joaoapereira21@hotmail.com>
2022-08-20 20:50:55 +0100

    Add solutions to the day 18 puzzles

---
+++ day18/part2.py
@@ -0,0 +1,17 @@
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import operator as op
+import re
+from collections import Counter
+
+with open("input") as inf, open("part2.out", "w+") as outf:
+    map = [tuple(c == "." for c in inf.readline().strip())]
+
+    while len(map) < 400000:
+        map.append(
+            tuple(not ((not l and not c and r) or (l and not c and not r) or
+                       (not l and c and r) or (l and c and not r))
+                  for l, c, r in mit.triplewise((True, *map[-1], True))))
+
+    outf.write(str(sum(sum(i) for i in map)))
